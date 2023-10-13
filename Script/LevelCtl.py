61b717c49cd46b8a0a02fdfea211139819781931
HOANGVU\hoang <thanhhoanghuyinco@gmail.com>
2023-10-13 20:38:00 +0700

    update LvCtl

--- Script/LevelCtl.py
+++ Script/LevelCtl.py
@@ -1,3 +1,6 @@
+import pygame
+
+import CONST
 from WaterPipe import Pipe
 
 
@@ -12,8 +15,9 @@ class FactoryLevel:
 
 
 class LevelCtl:
-    def __init__(self, pipe_manager):
+    def __init__(self, pipe_manager, is_create_new_pipe):
         self.pipe_manager = pipe_manager
+        self.is_create_new_pipe = is_create_new_pipe
         pass
 
     def update(self, screen):
@@ -25,13 +29,13 @@ class LevelCtl:
 
 class Level1(LevelCtl):
     def __init__(self, pipe_manager, speed):
-        super().__init__(pipe_manager)
+        super().__init__(pipe_manager, True)
         self.pipeSpeed = speed
 
     def update(self, screen):
         self.set_pile_in_screen()
         self.pipe_manager.remove_offscreen_pipes()
-        self.pipe_manager.draw_pipes(screen)
+        self.pipe_manager.draw_pipes(screen, self.pipe_manager.pipes)
 
     def set_pile_in_screen(self):
         for pipe in self.pipe_manager.pipes:
@@ -44,12 +48,21 @@ class Level1(LevelCtl):
 
 class Level2(LevelCtl):
     def __init__(self, pipe_manager):
-        super().__init__(pipe_manager)
+        super().__init__(pipe_manager, False)
+        for i in range(5):
+            new_pipe = Pipe(CONST.WIDTH - 50, 100 + i * 200, pipe_manager.pipe_image, 90)
+            self.pipe_manager.pipes.append(new_pipe)
+        for i in range(5):
+            new_pipe = Pipe(-100, 100 + i * 200, pipe_manager.pipe_image, -90)
+            new_pipe.x = -(new_pipe.height - 50)
+            self.pipe_manager.pipes.append(new_pipe)
+
+        self.pipe_item = 0
 
     def update(self, screen):
-        self.set_pile_in_screen()
-        self.pipe_manager.remove_offscreen_pipes()
-        self.pipe_manager.draw_pipes(screen)
+        list_pile_draw = self.pipe_manager.pipes[:5] if self.pipe_item == 0 else self.pipe_manager.pipes[5:]
+        self.pipe_manager.draw_pipes(screen, list_pile_draw)
 
     def set_pile_in_screen(self):
+        self.pipe_item = 0 if self.pipe_item == 1 else 1
         pass
