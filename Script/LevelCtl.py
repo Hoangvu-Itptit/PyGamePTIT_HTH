ec98c34d00b71cc5af2756cb5fa4b5c359a439dc
HOANGVU\hoang <thanhhoanghuyinco@gmail.com>
2023-10-13 22:28:33 +0700

    update LV2

--- Script/LevelCtl.py
+++ Script/LevelCtl.py
@@ -1,3 +1,5 @@
+import random
+
 import CONST
 from WaterPipe import Pipe
 
@@ -41,7 +43,6 @@ class Level1(LevelCtl):
 
     def move_pile(self, pipe: Pipe):
         pipe.y += self.pipeSpeed
-        pipe.rect.x = pipe.x
         pipe.pipe_rect.centery += self.pipeSpeed
 
 
@@ -49,20 +50,39 @@ class Level2(LevelCtl):
     def __init__(self, pipe_manager):
         super().__init__(pipe_manager, False)
         for i in range(5):
-            new_pipe = Pipe(CONST.WIDTH - 50, 100 + i * 200, pipe_manager.pipe_image, 90)
-            self.pipe_manager.pipes.append(new_pipe)
-        for i in range(5):
             new_pipe = Pipe(-100, 100 + i * 200, pipe_manager.pipe_image, -90)
             new_pipe.x = -(new_pipe.height - 50)
             new_pipe.pipe_rect.centerx = -(new_pipe.height - 50)
             self.pipe_manager.pipes.append(new_pipe)
-
+        self.set_pile_in_screen()
         self.pipe_item = 0
 
     def update(self, screen):
-        list_pile_draw = self.pipe_manager.pipes[:5] if self.pipe_item == 0 else self.pipe_manager.pipes[5:]
-        self.pipe_manager.draw_pipes(screen, list_pile_draw)
+        self.pipe_manager.draw_pipes(screen, self.pipe_manager.pipes)
 
     def set_pile_in_screen(self):
-        self.pipe_item = 0 if self.pipe_item == 1 else 1
+        self.pipe_manager.pipes = self.instance_pipe_left() if self.pipe_item == 0 else self.instance_pipe_right()
+        self.pipe_item = 1 if self.pipe_item == 0 else 0
         pass
+
+    def instance_pipe_left(self) -> []:
+        posY = [i * 100 for i in range(1, 10)]
+        kq = []
+        for i in range(5):
+            y = random.choice(posY)
+            posY.remove(y)
+            new_pipe = Pipe(CONST.WIDTH - 50, y, self.pipe_manager.pipe_image, 90)
+            kq.append(new_pipe)
+        return kq
+
+    def instance_pipe_right(self) -> []:
+        posX = [i * 100 for i in range(1, 10)]
+        kq = []
+        for i in range(5):
+            x = random.choice(posX)
+            posX.remove(x)
+            new_pipe = Pipe(-100, 100 + i * 200, self.pipe_manager.pipe_image, -90)
+            new_pipe.x = -(new_pipe.height - 50)
+            new_pipe.pipe_rect.centerx = -(new_pipe.height - 50)
+            kq.append(new_pipe)
+        return kq
